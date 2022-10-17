ff3a3d9ff51120f412879dd45ef07a3adad1fd74
hoanldph14732 <hoanldph14732@fpt.edu.vn>
2022-10-17 23:26:19 +0700

    format code

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -14,7 +14,7 @@ class CategoryController extends Controller
     public function save(CategoryRequest $request)
     {
         if (Category::insert($request->all())) {
-            return Response();
+            return true;
         }
         return false;
     }
--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -3,13 +3,14 @@
 namespace App\Http\Controllers;
 
 use App\Models\Product;
-use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Response;
+use Illuminate\Http\Response;
+
+;
 
 class ProductController extends Controller
 {
     public function index()
     {
-        return Response()->json(Product::paginate(10),200);
+        return response()->json(Product::paginate(10), 200);
     }
 }
