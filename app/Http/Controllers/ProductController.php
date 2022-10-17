d9df6095743542ded8ab0270281999edcbc9c15d
hoanldph14732 <hoanldph14732@fpt.edu.vn>
2022-10-18 01:30:01 +0700

    update file resource

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -2,14 +2,13 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Resources\CategoriesResource;
 use App\Models\Product;
-use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Response;
 
 class ProductController extends Controller
 {
     public function index()
     {
-        return Response()->json(Product::paginate(10),200);
+        return new CategoriesResource(Product::paginate(10));
     }
 }
