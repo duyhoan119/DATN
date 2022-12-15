38792f69c4ef8cdbdd19429f04703ae4bbf05d8c
HOCDT <babiicuteo@gmail.com>
2022-12-15 17:10:22 +0700

    CRUD user

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+use App\Models\User;
+use App\Models\Role;
+use App\Http\Requests\UserRequest;
+use App\Http\Requests\UpdateUserRequest;
+use App\Http\Resources\UserResource;
+use App\Http\Resources\UpdateUserResource;
+use Illuminate\Http\Request;
+
+class UserController extends Controller
+{
+    public function index(Request $request)
+    { 
+        $keyword = $request->get('keyword');
+
+        if ($keyword) {
+            return Response()->json(User::where('status', '=', 1)->where('name', 'like', '%' . $keyword . '%')->paginate(10), 200);
+        } else {
+            return Response()->json(User::where('status', '=', 1)->paginate(10), 200);
+        }
+    }
+
+    public function save(UserRequest $request)
+    { 
+        if (User::insert($request->all())) { 
+            if ($request->file('image')) { 
+                $user['image'] = $this->uploadFile($request->file('image'));
+            } 
+            return true;
+        }
+        return false;
+    }
+
+    public function uploadFile($file)
+    { 
+            $filename =  time() . '_' . $file->getClientOriginalName();
+            return $file->storeAs('imagesUser', $filename,  'public'); 
+    }
+
+    public function getUser($id)
+    {
+        return new UpdateUserResource(User::where('status', '=', 1)->find($id));
+    }
+
+    public function store($id, UpdateUserRequest $request)
+    {
+        if ($request->file('avatar')) { 
+            $user['avatar'] = $this->uploadFile($request->file('avatar'));
+        }   
+        return User::query()->find($id)->update($request->all());
+    }
+
+    public function delete($id) { 
+        if (!empty($id)) {
+            $User = User::where('id', '=', $id);
+            $data = [
+                'status' => 0
+            ];
+            $User->update($data);
+            return true;
+        }
+    } 
+}
